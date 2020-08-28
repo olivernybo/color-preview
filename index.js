e926364ad0283ab77867dd77392961e35c57789f
Oliver Nybo <root@olivernybo.com>
2020-08-28 23:43:30 +0200

    add colors and toggle fullscreen

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
-const { app, BrowserWindow, ipcMain } = require('electron')
+const { app, BrowserWindow, Menu, ipcMain } = require('electron')
+const prompt = require('electron-prompt')
 const Store = require('electron-store')
 const path = require('path')
 
 const data = new Store
 
 ipcMain.handle('colors:get', event => {
+	if (!data.has('colors')) return
 	const serialized = data.get('colors')
 	const colors = serialized ? JSON.parse(serialized) : []
 	event.sender.send('colors:update', ...colors)
 })
 
 ipcMain.handle('timer:get', event => {
+	if (!data.has('timer')) return
 	const ms = data.get('timer')
 	if (ms) event.sender.send('timer:update', Number(ms))
 })
@@ -23,5 +26,47 @@ app.whenReady().then(() => {
 			nodeIntegration: true
 		}
 	})
+
+	const menu = Menu.buildFromTemplate([
+		{
+			label: 'Edit',
+			submenu: [
+				{
+					label: 'Add Color',
+					accelerator: 'CmdOrCtrl+Plus',
+					click: () => prompt({
+						title: 'Add Color',
+						type: 'input',
+						inputAttrs: {
+							required: true
+						}
+					}).then(res => {
+						if (res) {
+							const colors = data.has('colors') ? JSON.parse(data.get('colors')) : []
+							colors.push(res)
+							data.set('colors', JSON.stringify(colors))
+							win.webContents.send('colors:new')
+						}
+					})
+				}
+			]
+		},
+		{
+			label: 'View',
+			submenu: [
+				{
+					label: 'Toggle Fullscreen',
+					accelerator: 'CmdOrCtrl+F',
+					click: () => win.setFullScreen(!win.isFullScreen())
+				}
+			]
+		},
+		{
+			label: 'Dev',
+			click: () => win.webContents.toggleDevTools()
+		}
+	])
+	Menu.setApplicationMenu(menu)
+
 	win.loadFile(path.join('ui', 'index.html'))
 })
--- ui/js/colors.js
+++ ui/js/colors.js
@@ -9,10 +9,12 @@ const timerFunc = () => {
 let timer = setInterval(timerFunc, 3000)
 
 ipcRenderer.on('colors:update', (_, ...newColors) => colors = newColors)
+ipcRenderer.on('colors:new', () => ipcRenderer.invoke('colors:get'))
 ipcRenderer.invoke('colors:get')
 
 ipcRenderer.on('timer:update', (_, ms) => {
 	clearInterval(timer)
 	timer = setInterval(timerFunc, ms)
 })
+ipcRenderer.on('timer:new', () => ipcRenderer.invoke('timer:get'))
 ipcRenderer.invoke('timer:get')
