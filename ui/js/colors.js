333040b0ee41f509e4f08714ef7b1ea46c12be35
Oliver Nybo <root@olivernybo.com>
2020-08-28 23:53:24 +0200

    show colors in the window

--- ui/js/colors.js
+++ ui/js/colors.js
@@ -1,14 +1,23 @@
 const { ipcRenderer } = require('electron')
 
 let colors = []
+let i = 0
 
 const timerFunc = () => {
-	console.log(colors)
+	if (colors.length <= i) return
+	const color = colors[i]
+	const body = document.querySelector('body')
+	body.style.backgroundColor = color
+	i++
+	if (colors.length <= i) i = 0
 }
 
 let timer = setInterval(timerFunc, 3000)
 
-ipcRenderer.on('colors:update', (_, ...newColors) => colors = newColors)
+ipcRenderer.on('colors:update', (_, ...newColors) => {
+	colors = newColors
+	timerFunc()
+})
 ipcRenderer.on('colors:new', () => ipcRenderer.invoke('colors:get'))
 ipcRenderer.invoke('colors:get')
 
